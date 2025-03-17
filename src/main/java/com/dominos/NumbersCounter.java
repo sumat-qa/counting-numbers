44ca682db013c5625d442010eb283f1b4c7fb537
sumat-qa <92343711+sumat-qa@users.noreply.github.com>
2025-03-17 16:04:50 -0400

    Initial Commit

---
+++ src/main/java/com/dominos/NumbersCounter.java
@@ -0,0 +1,16 @@
+package com.dominos;
+
+/*
+Given an integer collection, return an array with three elements:
+how many of them are positive, how many negative and how many are zeros.
+Given: [-4,3,-9,0,4,1] then expected output is: [3, 2, 1]
+ */
+
+import java.util.Arrays;
+
+public class NumbersCounter {
+
+  public int[] count(int[] numbers) {
+    //Write your code here
+  }
+}
